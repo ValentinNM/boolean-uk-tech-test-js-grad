b99fa1781b324ef1d84b1312a73d9f490e3af3a2
ValentinNM <valentinm.web@gmail.com>
2022-01-05 20:02:54 +0200

    major versions

--- src/01-count-major-versions-above-10/index.js
+++ src/01-count-major-versions-above-10/index.js
@@ -28,9 +28,37 @@ The results should have this structure:
  *  the number of packages that have a MAJOR semver version 
  *  greater than 10.x.x
  */
+const fetch = require('node-fetch');
 
-module.exports = async function countMajorVersionsAbove10() {
-  // TODO
+let data = [];
 
+fetch(`http://ambush-api.inyourarea.co.uk/ambush/intercept`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    url: "https://api.npms.io/v2/search/suggestions?q=react",
+    method: "GET",
+    return_payload: true
+  })
+})
+  .then(res => res.json())
+  .then(res => {
+    data = res.content;
+    console.log(data);
+  });
+  
+
+module.exports = async function countMajorVersionsAbove10(data) {
+
+  let count = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].version.split('.')[0] > 10) {
+      count++;
+    }
+  }
+  
   return count
 };
